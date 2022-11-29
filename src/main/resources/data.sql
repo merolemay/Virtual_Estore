70c1b93250ecf197252c74dc1afc7f9a82bbf892
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2022-11-28 22:42:18 -0500

    Resourses

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_role(role_id, role_name, description) VALUES ('2e72ed53-f5e2-4f7a-bd86-8aadcadeb4eb','admin', 'Base role for admin') ON CONFLICT DO NOTHING
+INSERT INTO user_permission(permission_id, uri, permission_key, permission_method) VALUES ('ccc7ff73-1989-413a-ab52-9bec7a049e33', '/users', 'create.user', 'POST') ON CONFLICT DO NOTHING
+INSERT INTO role_permission(role_id, permission_id) VALUES ('2e72ed53-f5e2-4f7a-bd86-8aadcadeb4eb', 'ccc7ff73-1989-413a-ab52-9bec7a049e33') ON CONFLICT DO NOTHING
