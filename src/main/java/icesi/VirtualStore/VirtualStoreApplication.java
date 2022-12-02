e167205f85f7d7cbd1e9ae57c7f90ad45c56c0fd
Dev.Pachon <dahp-dahp@hotmail.com>
2022-12-02 15:05:25 -0500

    Added some error handlers

--- src/main/java/icesi/VirtualStore/VirtualStoreApplication.java
+++ src/main/java/icesi/VirtualStore/VirtualStoreApplication.java
@@ -3,7 +3,10 @@ package icesi.VirtualStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableJpaRepositories
--- src/main/java/icesi/VirtualStore/service/impl/UserServiceImpl.java
+++ src/main/java/icesi/VirtualStore/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package icesi.VirtualStore.service.impl;
 
 
+import icesi.VirtualStore.constant.VirtualStoreErrorCode;
+import icesi.VirtualStore.error.exception.VirtualStoreError;
+import icesi.VirtualStore.error.exception.VirtualStoreException;
 import icesi.VirtualStore.model.Permission;
 import icesi.VirtualStore.model.Role;
 import icesi.VirtualStore.model.User;
@@ -9,6 +12,7 @@ import icesi.VirtualStore.repository.RoleRepository;
 import icesi.VirtualStore.repository.UserRepository;
 import icesi.VirtualStore.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,12 +32,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(UUID userId) {
-        return userRepository.findById(userId).orElse(null);
+        return userRepository.findById(userId).orElseThrow(()-> new VirtualStoreException(HttpStatus.NOT_FOUND, new VirtualStoreError(VirtualStoreErrorCode.CODE_U_01, VirtualStoreErrorCode.CODE_U_01.getMessage())));
     }
 
     @Override
     public User createUser(User userDTO, String roleName) {
-        Role role = roleRepository.findByName(roleName).orElseThrow();
+        Role role = roleRepository.findByName(roleName).orElseThrow(()-> new VirtualStoreException(HttpStatus.NOT_FOUND, new VirtualStoreError(VirtualStoreErrorCode.CODE_L_04, VirtualStoreErrorCode.CODE_L_04.getMessage())));
         userDTO.setRole(role);
         List<Permission> permissions = StreamSupport.stream(permissionRepository.findAll().spliterator(), false).collect(Collectors.toList());
         return userRepository.save(userDTO);
