af8315ce7c96f7864593e1e4eedbc44b5c9cdf12
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2022-12-02 12:17:27 -0500

    Password CA was missing so is also added

---
+++ src/main/java/icesi/VirtualStore/validation/PasswordValidator.java
@@ -0,0 +1,15 @@
+package icesi.VirtualStore.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<CustomAnnotations.ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        String nameRegex = "\"^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$\"";
+
+        return s.matches(nameRegex);
+    }
+}
+}
