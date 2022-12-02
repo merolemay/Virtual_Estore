0a8540d0e6812b50ad59fece67dff0892efbb794
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2022-12-02 15:53:30 -0500

    Service test integration test finished

--- src/test/java/icesi/VirtualStore/Integration/ItemServiceIntegrationTest.java
+++ src/test/java/icesi/VirtualStore/Integration/ItemServiceIntegrationTest.java
@@ -1,16 +1,34 @@
 package icesi.VirtualStore.Integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import icesi.VirtualStore.dto.ItemTypeDTO;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -34,6 +52,32 @@ public class ItemServiceIntegrationTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
+    @Test
+    @SneakyThrows
+    public void createItemSuccessfully() {
+        ItemTypeDTO itemTypeDTO = createItemDTO();
+        String body = objectMapper.writeValueAsString(itemTypeDTO);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isCreated())
+                .andReturn();
+        String response = result.getResponse().getContentAsString();
+        ItemTypeDTO responseDTO = objectMapper.readValue(response, ItemTypeDTO.class);
+        assertThat(responseDTO, hasProperty("name", is(itemTypeDTO.getName())));
+    }
 
+    @SneakyThrows
+    private ItemTypeDTO createItemDTO() {
+    String body = parseResourceToString("JsonFiles/createItem.json");
+    return objectMapper.readValue(body, ItemTypeDTO.class);
+    }
+    @SneakyThrows
+    private String parseResourceToString(String classPath) {
+        Resource resource = new ClassPathResource(classPath);
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        }
+    }
 
 }
